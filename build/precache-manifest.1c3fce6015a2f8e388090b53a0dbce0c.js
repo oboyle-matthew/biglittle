a828ab4fbb53fe60ba3ff8a07a5e636cb2e5172a
Matthew O'Boyle <oboyle.matthew@duke.edu>
2019-02-08 17:02:36 -0500

    Adding build folder

---
+++ build/precache-manifest.1c3fce6015a2f8e388090b53a0dbce0c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "092a5775c2388df4adc8",
+    "url": "/static/js/main.092a5775.chunk.js"
+  },
+  {
+    "revision": "a852b0ebc3c774f96352",
+    "url": "/static/js/1.a852b0eb.chunk.js"
+  },
+  {
+    "revision": "092a5775c2388df4adc8",
+    "url": "/static/css/main.d0f77732.chunk.css"
+  },
+  {
+    "revision": "fce53ae77a1d3efe584b8362ce6da9c8",
+    "url": "/index.html"
+  }
+];
